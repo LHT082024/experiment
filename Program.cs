060a3cf7e1cb089d625a7c574453fb8cc772ac9a
LHT082024 <LHT082024@gmail.com>
2024-10-29 10:55:27 +0100

    made intro message

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(@"Welcome to Hogwarts here you will learn magic or die or something
+the possibilites are endless :) but first lets create your character");
+        Console.WriteLine("but first what is your name?");
 
     }
 }
