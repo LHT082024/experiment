f07dc2a9292be1edd91d2204ffe1eec893435cbf
LHT082024 <LHT082024@gmail.com>
2024-10-29 10:22:05 +0100

    made classes

---
+++ Klasser/Model.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace experiment.Klasser
+{
+    public class Model
+    {
+        public string Species { get; set; }
+    }
+}
