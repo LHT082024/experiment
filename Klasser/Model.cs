6d7e5fac4930d991e39d94ac60eb80b381b84792
LHT082024 <LHT082024@gmail.com>
2024-10-29 10:52:01 +0100

    made some arrays for the lists

--- Klasser/Model.cs
+++ Klasser/Model.cs
@@ -7,12 +7,23 @@ namespace experiment.Klasser
 {
     public class Model
     {
+        public string Name { get; set; }
         public string BloodStatus { get; set; }
         public string Speciality { get; set; }
+        public string Weakness { get; set; }
         public int PowerLevel { get; set; }
         public string House { get; set; }
-        public string Name { get; set; }
+
+
+        string[] bloodStatus = { "Pureblood", "Halfblood", "Muggleborn" };
+        string[] speciality = { "Charms", "Curses", "Transfiguration", "Healing", "Jinxes", "Hexes", "Counter-Spells" };
+        int[] powerlevel = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        string[] houses = { "Slytherin", "Gryffindor", "Huffelpuff", "Ravenclaw" };
+
     }
+
+
+
 
     // public override string ToString()
     //     {
