9c47b8204131c22ae7f2e907d662410aa6a90be1
LHT082024 <LHT082024@gmail.com>
2024-10-29 10:31:37 +0100

    commented out public override to string to see if I actually need it

--- Klasser/Model.cs
+++ Klasser/Model.cs
@@ -7,6 +7,16 @@ namespace experiment.Klasser
 {
     public class Model
     {
-        public string Species { get; set; }
+        public string BloodStatus { get; set; }
+        public string Speciality { get; set; }
+        public int PowerLevel { get; set; }
+        public string House { get; set; }
+        public string Name { get; set; }
     }
+
+    // public override string ToString()
+    //     {
+    //         return $"Your name is: {Name}\n Your BloodStatus: {BloodStatus}\n Your Speciality: {Speciality}\n Your Powerlever: {PowerLevel}\n And your House is: {House}";
+
+    //     }
 }
